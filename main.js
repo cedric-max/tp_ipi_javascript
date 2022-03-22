f3a0045d2b68235f4b238227a3e45b33e7c045a3
cedric kadima <kadimacedric3@gmail.com>
2022-03-22 20:59:14 +0100

    delete specific item ok

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 /* Variables */
 const inputAdd = document.getElementById("toAdd");
 const btnDeleteOne = document.getElementById("deleteOne");
+const popup = document.getElementById("popup");
+const popupEnd = document.getElementById("closePopup");
+const toDelete = document.getElementById("toDelete");
+const btnDelete = document.getElementById("btnDelete");
+
 const listCourse = document.querySelector(".list");
 let li = document.createElement('li');
 
@@ -21,9 +26,9 @@ let course = [
  * Liste les items de la liste de courses
  */
 function listItem() {
+    console.log("list : "+course)
    for (let i  = 0; i <course.length; i++){
        if (course) {
-           console.log(course[i])
        }
        listCourse.innerHTML += `<li>${course[i]}</li>`;
    }
@@ -36,18 +41,59 @@ listItem();
  */
 function addItemTab(event) {
     event.preventDefault();
-    course = [];
+    listCourse.innerHTML = '';
     let newItemList = inputAdd.value.toLowerCase().trim();
     course.push(newItemList)
     listItem();
 }
 
 /**
+ * affiche la popoup de suppréssion d'element dans la liste
+ */
+btnDeleteOne.addEventListener("click", clickPopup)
+popupEnd.addEventListener("click", killPopup)
+btnDelete.addEventListener("click", deleteItem)
+
+function clickPopup(event) {
+    popup.classList.toggle("hide");
+    console.log("clickPop "+course)
+
+}function killPopup(event) {
+    popup.classList.toggle("hide");
+    console.log("killPop "+course)
+}
+
+/**
  * Spprime l'item par apport a son contenue
  */
 
+function compareValue(i){
+    if (i === toDelete.value){
+        console.log(course.indexOf(i))
+        course.splice(course.indexOf(i),"1")
+        listCourse.innerHTML = '';
+        listItem()
+    }else{
+        console.log("non")
+    }
+}
+
 function deleteItem(){
 
+    course.forEach(i => compareValue(i));
+    popup.classList.toggle("hide");
+
+    // for (let i  = 0; i <course.length; i++){
+    //     if ( course[i] != toDelete.value) {
+    //         console.log(course[i])
+    //         console.log(course[i].indexOf(course[i]));
+    //
+    //     }else {
+    //         // alert("le produit mentionée n'éxiste pas sur la liste de course")
+    //         console.log("else")
+    //     }
+    //
+    // }
 }
 
 /* Codes principales */
